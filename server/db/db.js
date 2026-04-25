aa41604824c6e21710776cb5fe851f59b175424e
PKB <parthivsuryakb@gmail.com>
2026-04-25 10:22:43 +0530

    deployment

--- server/db/db.js
+++ server/db/db.js
@@ -17,7 +17,7 @@ const __dirname  = path.dirname(__filename);
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED !== 'false' } : false,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED !== 'false' } : false,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
